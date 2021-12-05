e0cf2379a14ad87c160c07604c0504b71bca7f5e
9sanha <64877575+9sanha@users.noreply.github.com>
2021-12-05 20:06:50 +0900

    [추가] 백준 14681

--- src/backjun/level2/Main14681.java
+++ src/backjun/level2/Main14681.java
@@ -1,6 +1,29 @@
 package backjun.level2;
 
-//사분면 구하기
-public class Main14681 {
+import java.util.Scanner;
 
+//사분면 구하기
+// 2 \ 1
+//--------
+// 3 \ 4
+public class Main14681 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = Integer.parseInt(scanner.nextLine());
+        int y = Integer.parseInt(scanner.nextLine());
+
+        if (x > 0) {
+            if (y > 0) {
+                System.out.println(1);
+            } else {
+                System.out.println(4);
+            }
+        } else if (x < 0 ) {
+            if (y>0){
+                System.out.println(2);
+            }else {
+                System.out.println(3);
+            }
+        }
+    }
 }
