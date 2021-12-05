c78f917c4f25e0c8c8d084bbce05c1e661e40d23
9sanha <64877575+9sanha@users.noreply.github.com>
2021-12-05 21:09:56 +0900

    [추가] 백준 10950

---
+++ src/backjun/level3/Main10950.java
@@ -0,0 +1,16 @@
+package backjun.level3;
+
+import java.util.Scanner;
+
+public class Main10950 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        int a,b;
+        for (int i = 0; i<t;i++){
+            a = sc.nextInt();
+            b = sc.nextInt();
+            System.out.println(a+b);
+        }
+    }
+}
