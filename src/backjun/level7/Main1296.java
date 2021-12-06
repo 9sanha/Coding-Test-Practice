659c4f764ca5c4eab1d42609fc0e2b3c41fce1ef
9sanha <64877575+9sanha@users.noreply.github.com>
2021-12-06 12:56:48 +0900

    [추가] 백준 1296

---
+++ src/backjun/level7/Main1296.java
@@ -0,0 +1,39 @@
+package backjun.level7;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main1296 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int[] nArr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int m = 0,y=0;
+        for (int i : nArr) {
+            m+=min(i);
+            y+=yeong(i);
+        }
+        String w = m==y?"Y M "+m:(y>m?"M "+m:"Y "+y);
+        System.out.println(w);
+
+    }
+
+    private static int min(int i){
+        int res=0;
+        while (i>=0){
+            res+=15;
+            i-=60;
+        }
+        return res;
+    }
+    private static int yeong(int i){
+        int res=0;
+        while (i>=0){
+            res+=10;
+            i-=30;
+        }
+        return res;
+    }
+}
