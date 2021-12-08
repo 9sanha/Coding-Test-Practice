fc7a150b5125e70f50a01039b1919b43cf08d376
9sanha <64877575+9sanha@users.noreply.github.com>
2021-12-09 00:32:26 +0900

    [추가] 백준 1874

---
+++ src/backjun/lever12/Main1874.java
@@ -0,0 +1,46 @@
+package backjun.lever12;
+
+import java.io.*;
+import java.util.Objects;
+import java.util.Stack;
+
+public class Main1874 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder st = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+        int[] numArr = new int[n];
+
+        for (int i = 0; i <n ; i++) {
+            numArr[i] = Integer.parseInt(br.readLine());
+        }
+        int k = 0;
+        int j=1;
+        while (true){
+            if (k==n|| stack.size()>n){
+                break;
+            }
+            if (stack.isEmpty()){
+                stack.add(j);
+                st.append("+\n");
+                j++;
+            }else if (Objects.equals(stack.peek(), numArr[k])){
+                stack.pop();
+
+                st.append("-\n");
+                k++;
+            }else {
+                stack.add(j);
+                st.append("+\n");
+
+                j++;
+            }
+        }
+        if (stack.empty()){
+            System.out.println(st);
+        }else {
+            System.out.println("NO");
+        }
+    }
+}
