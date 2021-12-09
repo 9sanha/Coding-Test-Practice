60a21d27dbe7e8c7003e005daa3426e8ef9bb5f2
9sanha <64877575+9sanha@users.noreply.github.com>
2021-12-09 15:06:27 +0900

    [추가] 백준 1927

---
+++ src/backjun/level19/Main1927.java
@@ -0,0 +1,29 @@
+package backjun.level19;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main1927 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 우선순위 큐 ( 들어온 순서가 아닌 특정한 조건으로 우선 순위를 매겨 사용)
+        // 기본값은 오름차순
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int x = Integer.parseInt(br.readLine());
+            if (queue.isEmpty() && x == 0) {
+                System.out.println(0);
+            } else if (x == 0) {
+                System.out.println(queue.poll());
+            } else {
+                queue.add(x);
+            }
+        }
+    }
+}
+
+
